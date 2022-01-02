25f9f21aa25eabb35082599f2a37e05541fb2f68
timsonner <timsonner@gmail.com>
2022-01-02 06:12:18 +0000

    Create card view

---
+++ src/lib/card.svelte
@@ -0,0 +1,14 @@
+  <script>
+    import { fade } from 'svelte/transition';
+    export let cardItem
+</script>
+
+<a class="no-underline p-6 bg-gray-800 text-gray-500 text-center rounded-md shadow-lg hover:bg-green-700 border-2 border-gray-200 flex flex-col items-center"
+href={`/pokemon/${cardItem.id}`} transition:fade>
+<!-- padding 6, background gray, text gray, center text, rounded border, box shadow, flex, column flex, center items -->
+    <img class="h-40 w-40" src={cardItem.artworkUrl100} alt="Album art">
+<!-- <h2 class="uppercase text-2xl">{cardItem.trackName}</h2>
+<h2 class="uppercase text-2xl">{cardItem.artistName}</h2> -->
+<div class="mb-2 h-6 overflow-hidden pb-8">{cardItem.trackName}</div>
+        <div class="text-xs font-bold">{cardItem.artistName}</div>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+ import Card from "../lib/card.svelte"
+    let searchResults = [];
+    export var searchTerm = ``
+$: {
+    if (searchTerm) {
+        fetchResults(searchTerm)
+    }
+}
+    async function fetchResults(term) {
+        const itunesSearched = await fetch(`https://itunes.apple.com/search?term=${term}&entity=song`);
+      var res = await itunesSearched.json();
+      searchResults = res.results;
+      console.log(searchResults)
+    }
+    
+  </script>
+  
+  <section>
+      
+    <input
+	class="w-full flex rounded-md bg-white bg-opacity-20 p-1 border border-white border-opacity-30"
+	bind:value={searchTerm}
+	type="text"
+	placeholder="Search songs..."
+/>
+
+    <div class="py-4 gap-4 grid md:grid-cols-3 grid-cols-1">
+        {#each searchResults as item}
+            <p>
+                <Card cardItem={item} />
+            </p>
+        {/each}
+    </div>
+
+
+  </section>
